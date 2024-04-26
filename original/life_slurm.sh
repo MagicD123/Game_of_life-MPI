ed42c3f95594fa556d3960652d84f6dccf1e5b11
MagicD123 <127274704+MagicD123@users.noreply.github.com>
2024-04-26 21:35:18 +1000

    Add files via upload

---
+++ original/life_slurm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=life
+#SBATCH --partition=cosc    # running on 'cosc'
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=1G    # memory (MB)
+#SBATCH --time=0-00:01      # max time allowed (D-HH:MM)
+
+time ./life
+gprof life gmon.out > profile.txt
